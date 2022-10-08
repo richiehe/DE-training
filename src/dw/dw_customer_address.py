c99158d43cb0a2095b948d59980766e22996ba09
Zhihua Zhang <zhihua.zhang@thoughtworks.com>
2022-10-08 16:47:38 +0800

    add dw customer address

--- src/dw/dw_customer_address.py
+++ src/dw/dw_customer_address.py
@@ -7,23 +7,23 @@ from schema.dataset_schema import DATASET_SCHEMA
 from utils.db_utils import remove_dw_partition_data
 
 if __name__ == '__main__':
-    spark = SparkSession.builder.appName("dw_customer") \
+    spark = SparkSession.builder.appName("dw_customer_address") \
         .config("spark.jars", f'{Path.home()}/DE-training/driver/postgresql-42.5.0.jar') \
         .getOrCreate()
 
     df = spark.read.format("jdbc") \
         .option("driver", "org.postgresql.Driver") \
         .option("url", "jdbc:postgresql://localhost:5432/dwh") \
-        .option("dbtable", "ods_customer") \
+        .option("dbtable", "ods_customer_address") \
         .option("user", "dwh") \
         .option("password", "dwh") \
         .load()
 
     df = df.filter(col('processed_date') == '2022-09-19') \
-        .dropDuplicates(['customer_id'])
+        .dropDuplicates(['customer_id', 'address_id'])
 
     if df.count() > 0:
-        remove_dw_partition_data(df, 'customer', DATASET_SCHEMA.get('customer').get('partition_field'))
+        remove_dw_partition_data(df, 'customer_address', DATASET_SCHEMA.get('customer_address').get('partition_field'))
 
     df = df.withColumn("is_valid", when(df.processed_date == '2022-09-19', True).otherwise(False))
 
@@ -33,7 +33,7 @@ if __name__ == '__main__':
         .format("jdbc") \
         .option("driver", "org.postgresql.Driver") \
         .option("url", "jdbc:postgresql://localhost:5432/dwh") \
-        .option("dbtable", "dw_customer") \
+        .option("dbtable", "dw_customer_address") \
         .option("user", "dwh") \
         .option("password", "dwh") \
         .save()
